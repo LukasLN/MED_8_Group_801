7b80346e0aa16a4aaf4c3d20ae3d8824b268418b
Holm Krestian Leonard Bendixen <72729931+HBendi20@users.noreply.github.com>
2024-03-15 12:22:28 +0100

    Update Spaceship.cs

--- P8 Math Game/Assets/Spaceship.cs
+++ P8 Math Game/Assets/Spaceship.cs
@@ -1,18 +1,61 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class Spaceship : MonoBehaviour
 {
+    public int[] Cargotype = { 0, 0, 0 };
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        transform.position = new Vector3(GeneratePosCoordinate(50,80), GeneratePosCoordinate(50, 80), GeneratePosCoordinate(50, 80));
+
+        if (Random.Range(0, 2) < 0)
+        {
+            Cargotype[0] = Random.Range(0, 4);
+        }
+        if (Random.Range(0, 2) < 0)
+        {
+            Cargotype[1] = Random.Range(0, 4);
+        }
+        if (Random.Range(0, 2) < 0)
+        {
+            Cargotype[2] = Random.Range(0, 4);
+        }
+
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+
+    /// <summary>
+    /// used for generating ship positions range, usually 50-80
+    /// </summary>
+    /// <param name="min">
+    /// lower boundary of range for random int
+    /// </param>
+    /// <param name="max">
+    /// higher boundary of range for random int
+    /// </param>
+    /// <returns>
+    /// an int within range
+    /// </returns>
+    public int GeneratePosCoordinate(int min, int max)
+    {
+        int result;
+        int roll = Random.Range(min, max);
+        if (Random.Range(0, 2) > 0)
+        {
+            result = roll * -1;
+        }
+        else 
+        {
+            result = roll;
+        }
+
+        return result;
     }
 }
