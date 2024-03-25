501f5caf29d8c72bb16f5443702c0da5e6aad5d6
Holm Krestian Leonard Bendixen <72729931+HBendi20@users.noreply.github.com>
2024-03-25 12:26:26 +0100

    Pause dialogue creationg

---
+++ P8 Math Game/Assets/DialogueManager.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace AstroMath
+{
+    public class DialogueManager : MonoBehaviour
+    {
+        public Queue<string> sentences;
+        // Start is called before the first frame update
+        void Start()
+        {
+        
+        }
+
+    }
+}
