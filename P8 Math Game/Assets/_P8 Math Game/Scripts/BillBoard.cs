331acb6b782a3ad919ef28c18be452c48265f68b
Julius-Winther <Jwinth20@student.aau.dk>
2024-04-08 13:24:12 +0200

    Bloom effect cheat ;)

---
+++ P8 Math Game/Assets/_P8 Math Game/Scripts/BillBoard.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace AstroMath
+{
+    public class BillBoard : MonoBehaviour
+    {
+        // Start is called before the first frame update
+        void Start()
+        {
+        
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+        transform.LookAt(Camera.main.transform.position, -Vector3.up);
+        }
+    }
+}
