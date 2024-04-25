07ffe1765454233dc1b11ee6722dbb12e12cd206
Holm Krestian Leonard Bendixen <72729931+HBendi20@users.noreply.github.com>
2024-04-25 19:25:15 +0200

    Added Progress Manager

---
+++ P8 Math Game/Assets/ProgressManager.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace AstroMath
+{
+    public class ProgressManager : MonoBehaviour
+    {
+        //[SerializeField] GameObject bigRedButton;
+        [SerializeField] AudioPlayer holoAudioplayer;
+        public int step = 0;
+
+        // Update is called once per frame
+       void state()
+        {
+            switch (step)
+            {
+                case 0: //red button not pressed
+
+                    break;
+                case 1: //red button pressed
+                    holoAudioplayer.PlaySoundEffect("Tutorial1",true);
+                    break;
+                case 2: //correct pincode inserted
+                    holoAudioplayer.PlaySoundEffect("Tutorial2", true);
+                    break;
+                case 3: //complete minigame
+                    holoAudioplayer.PlaySoundEffect("Tutorial3", true);
+                    break;
+                case 4: //flight initiated
+                    holoAudioplayer.PlaySoundEffect("Flight", true);
+                    break;
+                case 5: // wrong direction vector
+                    holoAudioplayer.PlaySoundEffect("Success", true);
+                    break;
+                case 6: // wrong direction vector
+                    holoAudioplayer.PlaySoundEffect("Failed", true);
+                    break;
+            }
+        }
+    }
+}
